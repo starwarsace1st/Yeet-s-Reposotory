602375b479ae0ee04dac1a639aed57beaa22677d
starwarsace1st <102997422+starwarsace1st@users.noreply.github.com>
2022-04-07 11:31:03 -0400

    Add files via upload

---
+++ python text thing.py
@@ -0,0 +1,8 @@
+print("hello world")
+#this is a copy from example 14/14 from codeacademy that i have changed slightly
+skill_completed = str("Python Syntax")
+exercises_completed = 583
+#The amount of points for each exercise may change, because points don't exist yet
+points_per_exercise = 2
+point_total = exercises_completed * points_per_exercise
+print("I got " + str(point_total) + " points!")
